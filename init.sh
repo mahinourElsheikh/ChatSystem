e19155e8ba92e1a443f7a8b4215a9a2479a44ce8
mahinour.elsheikh <mahinour.elsheikh@pharos-solutions.de>
2021-11-28 14:56:30 +0200

    fix docker compose

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
-rake db:migrate 2>/dev/null || rake db:create db:migrate
+rake db:migrate  RAILS_ENV=test 2>/dev/null || rake db:create db:migrate  RAILS_ENV=test
 rspec
+rake db:migrate RAILS_ENV=development 2>/dev/null || rake db:create db:migrate RAILS_ENV=development
 rake elasticsearch:build_index
